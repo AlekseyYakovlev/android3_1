c99f4f5b7d153537a8c246e9e0e7aa204c6bf31c
AlekseyYakovlev <aleksey@yakovlev.spb.ru>
2018-05-28 17:20:23 +0300

    RxJava added

--- app/src/main/java/ru/geekbrains/android3_1/presenter/MainPresenter.java
+++ app/src/main/java/ru/geekbrains/android3_1/presenter/MainPresenter.java
@@ -3,6 +3,7 @@ package ru.geekbrains.android3_1.presenter;
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
+import io.reactivex.Observable;
 import ru.geekbrains.android3_1.model.CounterModel;
 import ru.geekbrains.android3_1.view.MainView;
 
@@ -14,24 +15,36 @@ public class MainPresenter extends MvpPresenter<MainView> {
         this.model = model;
     }
 
+    public Observable<String> buttonOneText;
+    public Observable<String> buttonTwoText;
+    public Observable<String> buttonThreeText;
+
     @Override
     protected void onFirstViewAttach() {
         super.onFirstViewAttach();
+        //buttonOneText = Observable.create(e -> e.onNext(someText));
+        buttonOneText = Observable.just("0");
+        buttonTwoText = Observable.just("0");
+        buttonThreeText = Observable.just("0");
     }
+
 
     public void buttonClick(int id) {
         if (id >= 0 && id <= 2) {
-            int value = model.calculate(0);
+            int value = model.calculate(id);
 
             switch (id) {
                 case 0:
                     getViewState().setButtonOneText(value + "");
+                    buttonOneText = Observable.just(value + "");
                     break;
                 case 1:
                     getViewState().setButtonTwoText(value + "");
+                    buttonTwoText = Observable.just(value + "");
                     break;
                 case 2:
                     getViewState().setButtonThreeText(value + "");
+                    buttonThreeText = Observable.just(value + "");
                     break;
             }
         }
--- app/src/main/java/ru/geekbrains/android3_1/view/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android3_1/view/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.android3_1.view;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
@@ -10,9 +11,12 @@ import com.arellomobile.mvp.presenter.ProvidePresenter;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import ru.geekbrains.android3_1.R;
 import ru.geekbrains.android3_1.model.CounterModel;
 import ru.geekbrains.android3_1.presenter.MainPresenter;
+import timber.log.Timber;
 
 public class MainActivity extends MvpAppCompatActivity implements MainView {
     @BindView(R.id.btn_one)
@@ -27,16 +31,96 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
     @InjectPresenter
     MainPresenter presenter;
 
+    Observer<String> buttonOneTextSetter = new Observer<String>() {
+        @Override
+        public void onSubscribe(Disposable d) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            buttonOne.setText(s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
+    };
+    Observer<String> buttonTwoTextSetter = new Observer<String>() {
+        @Override
+        public void onSubscribe(Disposable d) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            buttonTwo.setText(s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
+    };
+    Observer<String> buttonThreeTextSetter = new Observer<String>() {
+        @Override
+        public void onSubscribe(Disposable d) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            buttonThree.setText(s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
+    };
+    ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
     }
 
     @ProvidePresenter
     public MainPresenter provideMainPresenter() {
         return new MainPresenter(new CounterModel());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (presenter.buttonOneText == null) Log.d("rrrrrrrrr", "presentor is null");
+        else {
+            Log.d("rrrrrrrrr", "presentor is NOT null");
+
+            presenter.buttonOneText.subscribe(buttonOneTextSetter);
+            presenter.buttonTwoText.subscribe(buttonTwoTextSetter);
+            presenter.buttonThreeText.subscribe(buttonThreeTextSetter);
+        }
     }
 
     @OnClick({R.id.btn_one, R.id.btn_two, R.id.btn_three})
@@ -45,30 +129,37 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
         switch (button.getId()) {
             case R.id.btn_one:
                 id = 0;
+                presenter.buttonOneText.subscribe(buttonOneTextSetter);
                 break;
             case R.id.btn_two:
                 id = 1;
+                presenter.buttonTwoText.subscribe(buttonTwoTextSetter);
                 break;
             case R.id.btn_three:
                 id = 2;
+                presenter.buttonThreeText.subscribe(buttonThreeTextSetter);
                 break;
         }
 
         presenter.buttonClick(id);
     }
 
+
     @Override
     public void setButtonOneText(String text) {
-        buttonOne.setText(text);
+        Log.d("rrrrrrrrr", "1 - " + text);
+        Timber.d(text);
     }
 
     @Override
     public void setButtonTwoText(String text) {
-        buttonTwo.setText(text);
+        Log.d("rrrrrrrrr", "2 - " + text);
+        Timber.d(text);
     }
 
     @Override
     public void setButtonThreeText(String text) {
-        buttonThree.setText(text);
+        Log.d("rrrrrrrrr", "3 - " + text);
+        Timber.d(text);
     }
 }
